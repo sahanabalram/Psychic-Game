e2a89ad6d320e6c12a7967575852f67f5130c124
Sahana Balram <sahana2008balram@gmail.com>
2017-08-15 16:03:09 -0700

    add game code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,27 @@
-var userInput
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+var userGuess = " ";
+// Creating variables to hold the number of wins, losses. They start at 0.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var generate = getRandomArbitrary(96, 123);
+document.onkeypress = function (event) {
+    console.log(generate);
+    var userGuess = event.key;
+    if (generate === userGuess.charCodeAt(0)) {
+        wins++;
+        console.log("you won:", wins);
+        generate = getRandomArbitrary(96, 123);
+        guessesLeft = 9;
+    } else {
+        guessesLeft--;
+        if (guessesLeft === 0) {
+            console.log("lost game", losses);
+            losses++;
+            guessesLeft = 9;
+            generate = getRandomArbitrary(96, 123);
+        }
+    }
+};
