f7c54fef22f6ac5aed5d7a7742e70885f1e550fe
Sahana Balram <sahana2008balram@gmail.com>
2017-08-15 21:12:57 -0700

    final update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,27 @@ var userGuess = " ";
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var generate = getRandomArbitrary(96, 123);
+var generate = getRandomArbitrary(97, 122);
 document.onkeypress = function (event) {
-    console.log(generate);
+
     var userGuess = event.key;
     if (generate === userGuess.charCodeAt(0)) {
         wins++;
-        console.log("you won:", wins);
-        generate = getRandomArbitrary(96, 123);
+        generate = getRandomArbitrary(97, 122);
         guessesLeft = 9;
+        document.querySelector("#your-guess-so-far").innerHTML = " ";
     } else {
         guessesLeft--;
+        document.querySelector("#your-guess-so-far").innerHTML = document.querySelector("#your-guess-so-far").innerHTML + userGuess + " ,";
         if (guessesLeft === 0) {
             losses++;
-            console.log("lost game", losses);
             guessesLeft = 9;
-            generate = getRandomArbitrary(96, 123);
+            generate = getRandomArbitrary(97, 122);
+            document.querySelector("#your-guess-so-far").innerHTML = " ";
         }
     }
-    document.querySelector("#wins").innerHTML= wins;
-    document.querySelector("#losses").innerHTML= losses;
-    document.querySelector("#guesses-left").innerHTML= document.guessesLeft;
-    document.querySelector("#your-guess-so-far").innerHTML= wins;
+    document.querySelector("#wins").innerHTML = wins.toString();
+    document.querySelector("#losses").innerHTML = losses.toString();
+    document.querySelector("#guesses-left").innerHTML = guessesLeft.toString();
+
 };
